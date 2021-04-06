7158a17dbedd22d114f023c9c6f3f1d87f85a4a6
psyche <cl598@njit.edu>
2021-04-05 22:18:36 -0400

    upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,20 @@
 import TestJS from './TestJs.js';
 
+function jQuery() {
+
+}
+
+(function ($) {
+    $('#treesTable').DataTable( {
+        "ajax": "http://localhost:9080/api/v1/cities",
+        "columns": [
+            { "data": "id" },
+            { "data": "Index" },
+            { "data": "Girth_in" },
+            { "data": "Height_ft" },
+            { "data": "Volume_ft_3" },
+        ]
+    } );
+})(jQuery);
+
 TestJS();
---
+++ src/server.js
@@ -0,0 +1,33 @@
+'use strict';
+const express = require('express');
+const app = express();
+const open = require("open");
+
+app.use(express.static('docs'));
+open("http://localhost:9080");
+
+
+app.get('/api/v1/trees', function(req, res) {
+    const mysql = require('mysql')
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        port: '32000',
+        user: 'root',
+        password: 'root',
+        database: 'treesData'
+    });
+    connection.connect();
+
+    connection.query('SELECT * FROM treesImport', function (err, rows) {
+        if (err) throw err;
+        res.json({ "data": rows});
+    })
+    connection.end()
+});
+
+app.set('port', process.env.PORT || 8000);
+app.set('ip', process.env.NODEJS_IP || '127.0.0.1');
+
+app.listen(app.get('port'), function() {
+    console.log('%s: Node server started on %s ...', Date(Date.now()), app.get('port'));
+});
